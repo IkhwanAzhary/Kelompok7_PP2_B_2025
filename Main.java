307b4e83ffa3e0b1dfa7af600952af45add27144
IkhwanAzhary <ikhwanazhary9@gmail.com>
2025-12-25 09:55:40 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.TubesSituKami;
+
+import id.ac.TubesSituKami.View.mainApp;
+import javax.swing.SwingUtilities;
+
+public class Main {
+   
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new mainApp().setVisible(true));
+    }
+}
