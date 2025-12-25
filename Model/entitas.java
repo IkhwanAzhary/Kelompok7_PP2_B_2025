76410102f95ff0a76c2a7015a914d77afb31d110
NajranAlFaresy <najranalfaresy06@gmail.com>
2025-12-25 12:50:31 +0700

    feat: modul, menambahkan file Database dan Entitas

--- Model/entitas.java
+++ Model/entitas.java
@@ -2,8 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package id.ac.unpas.TubesSituKami.Model;
+package id.ac.unpas.praktikumpemograman2.7_PP2_B_2025;
 
+/**
+ *
+ * @author NAJRAN AL-FARESY
+ */
 public class entitas {
+     public static class Mahasiswa {
+        public String npm, nama, jurusan;
+        public Mahasiswa(String n, String m, String j) { this.npm = n; this.nama = m; this.jurusan = j; }
+    }
 
+    public static class Dosen {
+        public String nidn, nama, email;
+        public Dosen(String n, String m, String e) { this.nidn = n; this.nama = m; this.email = e; }
+    }
+
+    public static class MataKuliah {
+        public String kode, nama; public int sks;
+        public MataKuliah(String k, String n, int s) { this.kode = k; this.nama = n; this.sks = s; }
+    }
 }
--- Model/koneksiDB.java
+++ Model/koneksiDB.java
@@ -2,8 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package id.ac.unpas.TubesSituKami.Model;
+package id.ac.unpas.praktikumpemograman2.7_PP2_B_2025;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author NAJRAN AL-FARESY
+ */
 public class koneksiDB {
-
+    private static Connection mysqlconfig;
+    public static Connection configDB() throws SQLException {
+        try {
+            String url = "jdbc:mysql://localhost:3306/situ_kami";
+            String user = "root";
+            String pass = "";
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            mysqlconfig = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Koneksi Gagal: " + e.getMessage());
+        }
+        return mysqlconfig;
+    }
 }
